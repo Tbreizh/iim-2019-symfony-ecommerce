7a643b7e9aab74c3904dd24cfdb2c29f0e3b51fb
Tbreizh <guillotristan24@gmail.com>
2019-01-15 10:19:49 +0100

    add Template

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index/index.html.twig');
+    }
+
+    /**
+     * @Route("/empty", name="empty")
+     */
+    public function empty()
+    {
+        return $this->render('empty.html.twig');
+    }
+}
