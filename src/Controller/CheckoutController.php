13f27256cf834e625d1a05fa9a2018e510597875
Tbreizh <guillotristan24@gmail.com>
2019-01-31 23:51:49 +0100

    Fake payment OKh

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Form\CardType;
 use App\Model\Card;
+use App\Entity\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 /**
  * @Route("/checkout")
@@ -16,7 +19,7 @@ class CheckoutController extends AbstractController
     /**
      * @Route("/payment", name="checkout_payment", methods={"GET","POST"})
      */
-    public function payment(Request $request)
+    public function payment(Request $request, SessionInterface $session)
     {
         $card = new Card();
         $form = $this->createForm(CardType::class, $card);
@@ -24,6 +27,11 @@ class CheckoutController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // @todo process payment
+            $objectManager = $this->getDoctrine()->getManager();
+            $cart = new Cart();
+            $objectManager->persist($cart);
+            $objectManager->flush();
+            $session->set('cart', $cartId = $cart->getId());
         }
 
         return $this->render('checkout/payment.html.twig', [
